b5133b9aacc96ffb2b20090a2d6f651b1927b0fc
Mikang87 <62625135+Mikang87@users.noreply.github.com>
2025-12-21 15:59:27 +0900

    Delete modules directory

--- modules/user/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from app.core.database import Base
-
-class UserConfig(Base):
-    __tablename__ = "user"
-    
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, index=True, nullable=False)
-    password = Column(String(100), nullable=False)
